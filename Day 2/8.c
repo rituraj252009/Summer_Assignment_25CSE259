7c7b0fb41a68764cfc3a8d34f416e23a1de7ec82
rituraj252009 <riturajyadav253@gmail.com>
2026-07-11 18:36:22 +0530

    Add files via upload

---
+++ Day 2/8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, original, digit, reverse = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0) {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n = n / 10;
+    }
+
+    if (original == reverse)
+        printf("%d is a Palindrome number.\n", original);
+    else
+        printf("%d is not a Palindrome number.\n", original);
+
+    return 0;
+}
